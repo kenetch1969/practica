da1dfecd57d6215d7914959b582a9f4b82933b0f
gerardo cruz <kenetch@hotmail.com>
2019-11-30 17:52:54 -0600

    Initial Commit

---
+++ Practica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practica
+//
+//  Created by Juan Gerardo Cruz on 11/30/19.
+//  Copyright © 2019 Juan Gerardo Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
